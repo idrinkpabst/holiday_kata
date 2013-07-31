04a3976a1490ff975e865d66c7ee71f494e4b1d9
Jake Parks <jakob.parks@gmail.com>
2013-07-31 12:10:10 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolidayKata::Application.config.secret_token = '577174e849254443a1122332784d3a90722623b7575a25084576b23280d6df1c26dde7b6e54974bbcc2686520a0455df3f31dae44a5067c19144a6939c254b83'

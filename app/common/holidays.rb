4ba91a034d98427e3f3ce1ef48e4e80ef7872623
Jake Parks <jakob.parks@gmail.com>
2013-07-31 12:28:41 -0700

    add setup data

--- app/common/holidays.rb
+++ app/common/holidays.rb
@@ -1,3 +1,15 @@
 class Holidays
 
+# New Year's Day:               January 1st
+# Martin Luther King Jr. Day:   third Monday of January
+# Presidents Day:               third Monday of February
+# Memorial Day:                 last Monday of May
+# Good Friday:                  two days before Easter
+# Independence Day:             July 4th
+# Labor Day:                    first Monday of September
+# Thanksgiving Day:             fourth Thursday of November
+# Christmas Day:                December 25th
+
+
+
 end
--- spec/common/holidays_spec.rb
+++ spec/common/holidays_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
+
+# New Year's Day:                 Tuesday, Jan. 1, 2013
+# Martin Luther King Jr. Day:     Monday, Jan. 21, 2013
+# Presidents Day:                 Monday, Feb. 18, 2013
+# Good Friday:                    Friday, March 29, 2013
+# Memorial Day:                   Monday, May 27, 2013
+# Independence Day:               Thursday, July 4, 2013
+# Labor Day:                      Monday, Sept. 2, 2013
+# Thanksgiving Day:               Thursday, Nov. 28, 2013
+# Christmas:                      Wednesday, Dec. 25, 2013
+
 describe 'Holidays' do
-  it do
-    Holidays.new
-  end
+
 end
+
+
+
